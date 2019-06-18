679867696f7f652e6ffbb14308224061cc8dbb84
maximtosh <mr.maks.filippov.94@mail.ru>
2019-06-18 23:08:52 +0300

    Initial commit

---
+++ app/src/main/java/com/example/homework322/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.homework322;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.opengl.Visibility;
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static android.view.View.VISIBLE;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editExpression;
+
+    TextView one;
+    TextView two;
+    TextView tree;
+    TextView four;
+    TextView five;
+    TextView six;
+    TextView seven;
+    TextView eight;
+    TextView nine;
+    TextView zero;
+    TextView plus;
+    TextView minus;
+    TextView multiply; //умножение
+    TextView divide; //деление
+    TextView clear;
+    TextView changeSign;
+    TextView proc;
+
+    Button  engineerViewButton;
+    View    calcViewButtons;
+    View    engeneerCalcView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        engineerViewButton = findViewById(R.id.engineerButton);
+        calcViewButtons = findViewById(R.id.calc_buttons);
+        engeneerCalcView = findViewById(R.id.engineerCalc);
+        engineerViewButton.setOnClickListener(engineerViewListner);
+    }
+
+
+    private final View.OnClickListener engineerViewListner = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(calcViewButtons.getVisibility() == VISIBLE) {
+                calcViewButtons.setVisibility(View.GONE);
+                engeneerCalcView.setVisibility(VISIBLE);
+            }
+            else{
+                calcViewButtons.setVisibility(VISIBLE);
+                engeneerCalcView.setVisibility(View.GONE);
+            }
+        }
+    };
+
+}
